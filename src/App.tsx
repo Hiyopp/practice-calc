72a948d1a457f7f4e3ecf438c7aa40be1cc640e4
Hiyopp <2005jonghyun@gm.gist.ac.kr>
2024-07-13 21:31:30 +0900

    display modifying

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, {useState} from "react";
-import styled from "styled-components";
 import "./App.css";
 
-const center: string = 'display: flex; justify-content: center; align-items: center;';
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+
+const center = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const BonChae = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: colunm; 
+  flex-direction: colunm;
   width: 400px;
   height: 45rem;
   background: #191507;
@@ -28,7 +33,7 @@ const HwaMeon = styled.div`
 const GyelGwa = styled.div`
   color: white;
   font-size: 40px;
-  font-family: 'Digital Dismay';
+  font-family: "Digital Dismay";
 `;
 
 const Del = styled.button`
@@ -73,72 +78,97 @@ const JungAngJungLiuel = styled.div`
 type btsType = {
   id: number;
   value: string;
+};
+
+const bts: btsType[][] = [
+  [
+    { id: 1, value: "C" },
+    { id: 2, value: "7" },
+    { id: 3, value: "4" },
+    { id: 4, value: "1" },
+    { id: 5, value: "+/-" },
+  ],
+  [
+    { id: 6, value: "()" },
+    { id: 7, value: "8" },
+    { id: 8, value: "5" },
+    { id: 9, value: "2" },
+    { id: 10, value: "0" },
+  ],
+  [
+    { id: 11, value: "%" },
+    { id: 12, value: "9" },
+    { id: 13, value: "6" },
+    { id: 14, value: "3" },
+    { id: 15, value: "." },
+  ],
+  [
+    { id: 16, value: "/" },
+    { id: 17, value: "X" },
+    { id: 18, value: "-" },
+    { id: 19, value: "+" },
+    { id: 20, value: "=" },
+  ],
+] as const;
+
+let count = 1;
+
+let distinguishValue: number;
+
+function Distinguish() {
+  if (distinguishValue === 1)
+    distinguishValue = 1; //내용 <-여기서 숫자와 글자 나눠야 함.
+  else distinguishValue = 1; //내용 수정
 }
 
-let btsNaeYong: btsType[][]=[[{id: 1, value: 'C'}],[{id: 6, value: '()'}],[{id: 11, value: '%'}],
-[{id: 16, value: '/'},{id: 17, value: 'X'},{id: 18, value: '-'},{id: 19, value: '+'},{id: 20, value: '='}]];
-  for (let i = 0; i < 3; i++){
-    for (let k = 1; k < 5; k++){
-      if(k===4) {
-        switch(i) {
-          case 0:
-            btsNaeYong[i][k]={id: 5*(i+1), value: '+/-'};
-            break;
-          case 1:
-            btsNaeYong[i][k]={id: 5*(i+2), value: '0'};
-            break;
-          case 2:
-            btsNaeYong[i][k]={id: 5*(i+3), value: '.'};
-            break;
-        }
-      }
-      else btsNaeYong[i][k]={id: (k+1)+i*5, value: String((i+7)-(k-1)*3)};
+const App = () => {
+  const [hyeonJaeGab, setHyeonJaeGab] = useState<btsType[]>([]);
 
-    }
-  }
+  if (hyeonJaeGab[13])
+    setHyeonJaeGab(hyeonJaeGab.filter((st) => st.id !== hyeonJaeGab[0].id));
 
-const bts: btsType[][] = new Array(4);                //bts[[5],[5],[5],[5]] 또는 bts[4][5]
-for (let i = 0; i < 4; i++) {
-    bts[i] = new Array(5);
-    for (let k = 0; k < 5; k++){
-      bts[i][k] = btsNaeYong[i][k];
-    }
-}
-
-const App: React.FC = () => {
-
-  const [hyeonJaeGab, setHyeonJaeGab] = useState<string[]>([]);
-
-  function Click(b: string) {
-    hyeonJaeGab[9] ? setHyeonJaeGab(hyeonJaeGab.filter(st => st !== '9')) :  //여기도 위 배열처럼 id로 설정
-    hyeonJaeGab[0] ? setHyeonJaeGab([...hyeonJaeGab,b]) : setHyeonJaeGab([b]);
+  function Click(i: number, b: string) {
+    setHyeonJaeGab([...hyeonJaeGab, { id: count, value: b }]); //useState의 set은 함수가 끝나면 설정된다.
+    count++;
+    distinguishValue = i;
   }
 
   function GyeGwaGab() {
     const [giulGwaGab, setGiulGwaGab] = useState();
 
-    const plus = () => {
-    }
-
+    const plus = () => {};
   }
 
   return (
-    <BonChae> 
-      <HwaMeon>{hyeonJaeGab.map((hJG) => (   //key id로 바꾸기
-        <GyelGwa key={hJG}>
-          {hJG}
-        </GyelGwa>))}
-        <Del>
-          del
-        </Del>
+    <BonChae>
+      <HwaMeon>
+        {hyeonJaeGab.map(
+          (
+            hJG, //key id로 바꾸기
+          ) => (
+            <GyelGwa key={hJG.id}>{hJG.value}</GyelGwa>
+          ),
+        )}
+        <Del>del</Del>
       </HwaMeon>
-      <YeepLiuck>{bts.map((bt)=>(
-        <JungAngJungLiuel key={bt[0].id}>{bt.map((b)=>(
-          <CalcBt key={b.id} onClick={() => {Click(b.value)}}>{b.value}</CalcBt>))}
-        </JungAngJungLiuel>))}
+      <YeepLiuck>
+        {bts.map((bt) => (
+          <JungAngJungLiuel key={bt[0].id}>
+            {bt.map((b) => (
+              <CalcBt
+                key={b.id}
+                onClick={() => {
+                  Click(b.id, b.value);
+                }}
+              >
+                {b.value}
+              </CalcBt>
+            ))}
+          </JungAngJungLiuel>
+        ))}
       </YeepLiuck>
     </BonChae>
-);
-}
+  );
+};
 
 export default App;
