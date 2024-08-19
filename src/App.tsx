777fed03ef81ff86028fd54ee5f4efae1e848bcc
Hiyopp <2005jonghyun@gm.gist.ac.kr>
2024-08-19 18:43:16 +0900

    Refactor: 코드 가독성 업그레이드

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
-import styled, { css } from "styled-components";
+import { useState } from "react";
+import styled from "styled-components";
 
-const center = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { btsType } from "./App.const";
+import { Display } from "./components/Display";
+import { InputValue } from "./components/InputValue";
 
 const BonChae = styled.div`
   display: flex;
@@ -19,153 +17,13 @@ const BonChae = styled.div`
   position: relative;
 `;
 
-const HwaMeon = styled.div`
-  ${center};
-  width: 86%;
-  height: 8rem;
-  background: #646cff;
-  position: absolute;
-  top: 2rem;
-`;
-
-const GyelGwa = styled.div`
-  color: white;
-  font-size: 40px;
-  font-family: "Digital Dismay";
-`;
-
-const Del = styled.button`
-  ${center};
-  width: 40px;
-  height: 20px;
-  padding: 0;
-  background: #646cff;
-  border: 2px solid black;
-  position: absolute;
-  bottom: 10px;
-  right: 10px;
-  color: black;
-`;
-
-const YeepLiuck = styled.div`
-  ${center};
-  width: 86%;
-  height: 30rem;
-  position: absolute;
-  bottom: 3rem;
-  background: #d6d6d6;
-  border-radius: 2%;
-`;
-
-const CalcBt = styled.button`
-  width: 3.8rem;
-  height: 3.8rem;
-  border-radius: 50%;
-  margin-bottom: 25%;
-  background: #f5f5f7;
-  padding: 0;
-  font-size: 23px;
-`;
-
-const JungAngJungLiuel = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 3%;
-`;
-
-type btsType = {
-  id: number;
-  value: string;
-  type: valueType;
-};
-
-enum valueType {
-  refresh,
-  number,
-  operation,
-  equal,
-}
-
 function App() {
-  const bts: btsType[][] = [
-    [
-      { id: 1, value: "C", type: valueType.refresh },
-      { id: 2, value: "7", type: valueType.number },
-      { id: 3, value: "4", type: valueType.number },
-      { id: 4, value: "1", type: valueType.number },
-      { id: 5, value: "+/-", type: valueType.operation },
-    ],
-    [
-      { id: 6, value: "()", type: valueType.operation },
-      { id: 7, value: "8", type: valueType.number },
-      { id: 8, value: "5", type: valueType.number },
-      { id: 9, value: "2", type: valueType.number },
-      { id: 10, value: "0", type: valueType.number },
-    ],
-    [
-      { id: 11, value: "%", type: valueType.operation },
-      { id: 12, value: "9", type: valueType.number },
-      { id: 13, value: "6", type: valueType.number },
-      { id: 14, value: "3", type: valueType.number },
-      { id: 15, value: ".", type: valueType.operation },
-    ],
-    [
-      { id: 16, value: "/", type: valueType.operation },
-      { id: 17, value: "X", type: valueType.operation },
-      { id: 18, value: "-", type: valueType.operation },
-      { id: 19, value: "+", type: valueType.operation },
-      { id: 20, value: "=", type: valueType.equal },
-    ],
-  ] as const;
-
-  let distinguishValue: number;
-
   const [hyeonJaeGab, setHyeonJaeGab] = useState<btsType[]>([]);
-
-  if (hyeonJaeGab[13])
-    setHyeonJaeGab(hyeonJaeGab.filter((st) => st.id !== hyeonJaeGab[0].id));
-
-  function Distinguish() {
-    if (distinguishValue === 1)
-      distinguishValue = 1; //내용 <-여기서 숫자와 글자 나눠야 함.
-    else distinguishValue = 1; //내용 수정
-  }
-
-  function Click(i: number, b: string, t: valueType) {
-    setHyeonJaeGab([...hyeonJaeGab, { id: i, value: b, type: t }]); //useState의 set은 함수가 끝나면 설정된다.
-    distinguishValue = i;
-  }
-
-  function GyeGwaGab() {
-    const [giulGwaGab, setGiulGwaGab] = useState();
-
-    const plus = () => {};
-  }
 
   return (
     <BonChae>
-      <HwaMeon>
-        {hyeonJaeGab.map((hJG) => (
-          <GyelGwa key={hJG.id}>{hJG.value}</GyelGwa>
-        ))}
-        <Del>del</Del>
-      </HwaMeon>
-      <YeepLiuck>
-        {bts.map((bt, index) => (
-          <JungAngJungLiuel key={index}>
-            {bt.map((b) => (
-              <CalcBt
-                key={b.id}
-                onClick={() => {
-                  Click(b.id, b.value, b.type);
-                }}
-              >
-                {b.value}
-              </CalcBt>
-            ))}
-          </JungAngJungLiuel>
-        ))}
-      </YeepLiuck>
+      <Display hyeonJaeGab={hyeonJaeGab} setHyeonJaeGab={setHyeonJaeGab} />
+      <InputValue hyeonJaeGab={hyeonJaeGab} setHyeonJaeGab={setHyeonJaeGab} />
     </BonChae>
   );
 }
---
+++ src/components/InputValue.tsx
@@ -0,0 +1,62 @@
+import { Dispatch, SetStateAction } from "react";
+import { center } from "src/App.const";
+import { bts, btsType, valueType } from "src/App.const";
+import styled from "styled-components";
+
+const YeepLiuck = styled.div`
+  ${center};
+  width: 86%;
+  height: 30rem;
+  position: absolute;
+  bottom: 3rem;
+  background: #d6d6d6;
+  border-radius: 2%;
+`;
+
+const CalcBt = styled.button`
+  width: 3.8rem;
+  height: 3.8rem;
+  border-radius: 50%;
+  margin-bottom: 25%;
+  background: #f5f5f7;
+  padding: 0;
+  font-size: 23px;
+`;
+
+const JungAngJungLiuel = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 3%;
+`;
+
+export function InputValue({
+  hyeonJaeGab,
+  setHyeonJaeGab,
+}: {
+  hyeonJaeGab: btsType[];
+  setHyeonJaeGab: Dispatch<SetStateAction<btsType[]>>;
+}) {
+  function click(i: number, b: string, t: valueType) {
+    if (hyeonJaeGab.length > 13) hyeonJaeGab.shift();
+    setHyeonJaeGab([...hyeonJaeGab, { id: i, value: b, type: t }]); //useState의 set은 함수가 끝나면 설정된다.
+  }
+
+  return (
+    <YeepLiuck>
+      {bts.map((bt, index) => (
+        <JungAngJungLiuel key={index}>
+          {bt.map((b) => (
+            <CalcBt
+              key={b.id}
+              onClick={() => {
+                click(b.id, b.value, b.type);
+              }}
+            >
+              {b.value}
+            </CalcBt>
+          ))}
+        </JungAngJungLiuel>
+      ))}
+    </YeepLiuck>
+  );
+}
